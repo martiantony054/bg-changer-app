e59273993e230beced3e352e7f478cd4ac23fd42
martinantony054 <martinantony054@gmail.com>
2024-10-21 21:13:48 +0530

    created bgchanger app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import {
+  Button
+} from "@material-tailwind/react";
+function App() {
+  const [clr,setClr] = useState('white')
+  
+  return (
+    <>
+     <div className='w-full h-screen' style={{backgroundColor:clr}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0'>
+       <div className='flex flex-wrap justify-center gap-3  rounded-md'>
+            <Button className='bg-orange-800 ' onClick={()=>setClr("orange")}>Orange</Button>
+            <Button className='bg-blue-700' onClick={()=>setClr("blue")}>Blue</Button>
+            <Button className='bg-green-700'onClick={()=>setClr("green")}>Green</Button>
+            
+       </div>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
